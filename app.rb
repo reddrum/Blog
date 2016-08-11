472ea997086701f41ff1002f84c43b6cb605714a
“reddrum” <anton.kranz@me.com>
2016-08-11 18:52:29 +0300

    index: get list of posts from database

--- app.rb
+++ app.rb
@@ -32,7 +32,10 @@ configure do
 end
 
 get '/' do
-	erb :index		
+  # выбираем список постов из БД
+  @results = @db.execute 'select * from Posts order by id desc'
+
+  erb :index
 end
 
 get '/new' do
