db1f4d878eb94f50b3250343b15b2a6c2b1ac936
“reddrum” <anton.kranz@me.com>
2016-08-11 19:23:04 +0300

    Add textarea to /details

--- app.rb
+++ app.rb
@@ -61,9 +61,13 @@ end
 
 # вывод информации о посте
 get '/details/:post_id' do
+  # получаем переменную из url
 	post_id = params[:post_id]
 
+  # получаем список постов(будет только один пост)
 	results = @db.execute 'select * from Posts where id = ?', [post_id]
+
+  # выбираем только один пост в переменную row
   @row = results[0]
 
   erb :details
